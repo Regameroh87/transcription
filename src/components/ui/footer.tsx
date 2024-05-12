a6ea54f5a433890e4bb710334629fc9c1a3a5756
Rodrigo Gamero <rodrigogamero@MacBook-Air-de-Rodrigo.local>
2024-05-11 21:28:53 -0300

    ajustando footer

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className=" flex w-full h-4 py-4 mb-4 justify-center">
+    <section className=" flex w-full h-4 py-4 my-4 justify-center">
       <h6>Hecho con ❤️ por Rodrigo Gamero</h6>
     </section>
   );
